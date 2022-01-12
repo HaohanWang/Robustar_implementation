7ff628f1a07e7f1e6e4f5a4f906e05b9fa1337cd
LinjingSUN <scyls1@nottingham.edu.cn>
2022-01-12 19:16:39 +0800

    few ui changes

--- front-end/packages/robustar/src/views/Config.vue
+++ front-end/packages/robustar/src/views/Config.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="d-flex flex-column align-center">
-    <table border="1" cellspacing="2" cellpadding="0">
+  <div>
+    <table class="table">
       <tr v-for="(value, key) of configs">
-        <th class="key">{{key}}</th>
-        <td class="value">{{value}}</td>
+        <th class="key">{{ key }}</th>
+        <td class="value">{{ value }}</td>
       </tr>
     </table>
   </div>
@@ -15,27 +15,56 @@ export default {
   name: 'Config',
   data() {
     return {
-      configs:null,
+      configs: null,
+      name: null,
     };
   },
   methods: {
-    getConfigSuccess(res){
-        console.log(res);
-        this.configs = res.data.data;
+    getConfigSuccess(res) {
+      console.log(res);
+      this.configs = res.data.data;
     },
-    getConfigFailed(res){
-        console.log(res);
-        this.configs = undefined;
+    getConfigFailed(res) {
+      console.log(res);
+      this.configs = undefined;
     },
     startGettingConfig() {
-      APIGetConfig(
-        this.getConfigSuccess,
-        this.getConfigFailed
-      );
+      APIGetConfig(this.getConfigSuccess, this.getConfigFailed);
+    },
+    changeName() {
+      if (configs.key == batch_size) {
+        name = 'Batch Size';
+        configs.key = name;
+      }
     },
   },
-  beforeMount(){
-      this.startGettingConfig();
-  }
+  beforeMount() {
+    this.startGettingConfig();
+    this.changeName();
+  },
 };
 </script>
+
+<style>
+.table {
+  margin: auto;
+  border-top: 1px solid #e6eaee;
+  border-left: 1px solid #e6eaee;
+}
+.key {
+  width: 200px;
+  background-color: #eff3f6;
+  color: #393c3e;
+  border-bottom: 1px solid #e6eaee;
+  border-right: 1px solid #e6eaee;
+}
+.value {
+  width: 250px;
+  height: 35px;
+  line-height: 35px;
+  box-sizing: border-box;
+  padding: 0 10px;
+  border-bottom: 1px solid #e6eaee;
+  border-right: 1px solid #e6eaee;
+}
+</style>
