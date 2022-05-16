f57a7619339048c120662c169ee102e2b13f6bbd
Leon-Leyang <scylh6@nottingham.edu.cn>
2022-05-16 21:40:06 +0800

    Test regular training and paired training

--- back-end/tests/test_app.py
+++ back-end/tests/test_app.py
@@ -348,3 +348,46 @@ class TestTrain:
 
         # Check the improvement of accuracy
         assert server.getTrainedAcc() > server.getInitAcc()
+
+        # Test paired training
+        data = {
+            'info': 'placeholder',
+            'configs': {
+                'model_name': 'test-regular-training',
+                'weight': '',
+                'train_path': '/Robustar2/dataset/train-10',
+                'test_path': '/Robustar2/dataset/test-10',
+                'class_path': './model/cifar-class.txt',
+                'port': '8000',
+                'save_dir': '/Robustar2/checkpoints',
+                'use_paired_train': True,
+                'mixture': 'random_pure',
+                'paired_data_path': '/Robustar2/dataset/paired-10',
+                'auto_save_model': True,
+                'batch_size': '27',
+                'shuffle': True,
+                'learn_rate': 0.003,
+                'pgd': 'no PGD',
+                'paired_train_reg_coeff': 0.001,
+                'image_size': 32,
+                'epoch': 5,
+                'thread': 4,
+                'pretrain': False,
+                'user_edit_buffering': False,
+                'save_every': 1
+            }
+        }
+
+        rv = client.post("/train", json=data).get_json()
+        assert rv['code'] == 0
+        assert rv['data'] == 'Training started!'
+        assert rv['msg'] == 'Success'
+
+        # Wait for the training
+        time.sleep(30)
+
+        print(server.getInitAcc())
+        print(server.getTrainedAcc())
+
+        # Check the improvement of accuracy
+        assert server.getTrainedAcc() > server.getInitAcc()
